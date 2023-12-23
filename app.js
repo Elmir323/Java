9d2c37e0bdd6c5f0b6dcfc209fbfcb39d7694ac0
Java <342@gmail>
2023-12-23 19:26:11 +0300

    Java

---
+++ app.js
@@ -0,0 +1,108 @@
+/* Pag-1 Не сделано*/
+
+/* var a = +prompt('Введите свой возраст');
+
+if(a > 0 || a == 18) {
+    alert('Вы молоды еще, нужно учиться');
+}else if(a > 18 || a == 50 ) {
+    alert('Вам скоро на пенсию');
+}else if(a > 50 || a ==59 ) {
+    alert('Вы пенсионер');
+}else if(a > 59 || a == 100) {
+    alert('Что-то пошло не так');
+} */
+
+
+/* Pag-2 */
+
+/* var clock = +prompt("Введите время")
+
+switch (clock) {
+    case 1:
+        alert('2 Часа ночи')
+        break;
+    case 2:
+        alert('2 Часа ночи')
+        break;
+    case 3:
+        alert('2 Часа ночи')
+        break;
+    case 4:
+        alert('2 Часа ночи')
+        break;
+    case 5:
+        alert('2 Часа ночи')
+        break;
+    case 6:
+        alert('2 Часа ночи')
+        break;
+    case 7:
+        alert('10 Часов утра')
+        break;
+    case 8:
+        alert('10 Часов утра')
+        break;
+    case 9:
+        alert('10 Часов утра')
+        break;
+    case 10:
+        alert('10 Часов утра')
+        break;
+    case 11:
+        alert('3 Часа дня')
+        break;
+    case 12:
+        alert('3 Часа дня')
+        break;
+    case 13:
+        alert('3 Часа дня')
+        break;
+    case 14:
+        alert('3 Часа дня')
+        break;
+    case 15:
+        alert('3 Часа дня')
+        break;
+    case 16:
+        alert('3 Часа дня')
+        break;
+    case 17:
+        alert('3 Часа дня')
+        break;
+    case 18:
+        alert('9 Часов вечера')
+        break;
+    case 19:
+        alert('9 Часов вечера')
+        break;
+    case 20:
+        alert('9 Часов вечера')
+        break;
+    case 21:
+        alert('9 Часов вечера')
+        break;
+    case 22:
+        alert('9 Часов вечера')
+        break;
+    case 23:
+        alert('9 Часов вечера')
+        break;
+} */
+
+/* Page-3 */
+
+
+/* var a = +prompt('Введите первое число')
+var b = +prompt('Введите второе число')
+var c = +prompt('Введите третье число')
+
+if(a < b || a < c) {
+    alert('Среднее число:' + a)
+}else if(b < a || b < c) {
+    alert('Среднее число:' + b)
+}else if(c < a || c < b) {
+    alert('Среднее число:' + c)
+}else {
+    alert('Ошибка...')
+} */
+
